a098d4b674cb7d848e65ce3fbce23fbb98de8d41
Terminator163 <arsenyrud@yandex.ru>
2020-12-21 17:11:20 +0300

    Фаратиананананананананананананананананнан тЫ нЕ ТЫ кОГДА ГОЛОДЕН

---
+++ venv/nemazice.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+date = input().split()
+x = before_the_concert(date[0], date
+8ehsioerhgihgiohoiesghoieshigheosgergihgoihore
+
+date = input().split()
+x = before_the_concert(date[0], date
+
+
+date = input().split()
+x = before_the_concert(date[0], date
